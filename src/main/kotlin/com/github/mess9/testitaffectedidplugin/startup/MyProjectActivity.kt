10d6bbea5e5443f7ccb54c08088022eb284bf261
GitHub Action <action@github.com>
2025-05-06 09:34:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/mess9/testitaffectedidplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/mess9/testitaffectedidplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.mess9.testitaffectedidplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
